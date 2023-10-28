889c5220774fa3c53ec7c9b5f2e4189075a5be55
Manan Suthar <90760973+manans07@users.noreply.github.com>
2023-10-28 13:26:45 +0530

    Add files via upload

---
+++ build/src/java/com/tech/blog/helper/ConnectionProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tech.blog.helper;
+
+import java.sql.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class ConnectionProvider {
+    
+    private static Connection con;
+    
+    public static Connection getConnection()
+    {
+        
+        try {
+            
+        if(con==null){   /*Register Driver Class*/
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            /*Creating Connection Object*/
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/exploretech","root","Oncethereisacat.");
+        }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        return con; 
+    }
+    
+}
